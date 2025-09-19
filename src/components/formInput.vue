12578b940c5d2b8f8ce06d0595b33352a1989bdd
chyxxnc <ahnchaeyeon7@gmail.com>
2025-09-19 17:05:32 +0900

    feat: post로 strapi에 값 저장 가능하도록 구현

--- src/components/formInput.vue
+++ src/components/formInput.vue
@@ -1,9 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import FormButton from './formButton.vue'
+
+const titleInput = ref('')
+const memoInput = ref('')
+
+const emit = defineEmits(['send'])
+
+const submitForm = () => {
+  const send = {
+    title: titleInput.value,
+    memo: memoInput.value,
+  }
+  emit('send', send)
+
+  // 값 초기화 시키기
+  titleInput.value = ''
+  memoInput.value = ''
+}
+</script>
+
 <template>
   <div>
     <div class="components-input-demo-size">
-      <a-input size="large" placeholder="Title" />
+      <a-input v-model:value="titleInput" size="large" placeholder="Title" />
     </div>
     <br />
-    <a-textarea placeholder="Memo" :rows="4" />
+    <a-textarea v-model:value="memoInput" placeholder="Memo" :rows="4" />
   </div>
+  <br />
+  <FormButton @click="submitForm" />
 </template>
--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
 import FormInput from '../components/formInput.vue'
-import FormButton from '../components/formButton.vue'
+const API_URL = import.meta.env.VITE_API_URL
+
+const handelSubmit = async (formData: any) => {
+  try {
+    const response = await fetch(`${API_URL}/api/forms`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        data: formData,
+      }),
+    })
+    if (response.ok) console.log('데이터 저장 성공')
+    else console.log('데이터 저장 실패')
+  } catch (error) {
+    console.log(error)
+  }
+}
 </script>
 
 <template>
   <main>
-    <FormInput />
-    <br />
-    <FormButton />
+    <FormInput @send="handelSubmit" />
   </main>
 </template>
